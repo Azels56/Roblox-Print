4d879c81ac7f30a2f3f1e41aaf7b15e8d4e75710
Watermelon <ejemmanuel45@gmail.com>
2022-07-28 12:52:26 +0800

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,43 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#define x(x) (x - 0x400000)
+
+void console(const char* title) {
+	DWORD old;
+	VirtualProtect(reinterpret_cast<PVOID>(&FreeConsole), 1, PAGE_EXECUTE_READWRITE, &old);
+	*reinterpret_cast<std::uint8_t*>(&FreeConsole) = 0xC3;
+	VirtualProtect(&FreeConsole, 1, old, &old);
+	AllocConsole();
+	SetConsoleTitleA(title);
+	FILE* t_dummy = new FILE{};
+	freopen_s(&t_dummy, "CONOUT$", "w", stdout);
+	freopen_s(&t_dummy, "CONOUT$", "w", stderr);
+	freopen_s(&t_dummy, "CONIN$", "r", stdin);
+}
+
+void main()
+{
+	console("test");
+	// Singeton Print
+	const auto print = reinterpret_cast<int(__cdecl*)(int, const char*, ...)>(reinterpret_cast<std::uintptr_t>(GetModuleHandle(nullptr)) + x(0x805230));
+	std::string str;
+	int type;
+	std::cout << "0 - Regular, 1 - Info, 2 - Warn, 3 - Error" << std::endl;
+	while (true) {
+		std::cout << "Type: ";
+		std::cin >> type;
+		std::cout << "Print: ";
+		std::cin >> str;
+		const char* c = str.c_str();
+		print(type, c);
+	}
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+		std::thread(main).detach();
+	return TRUE;
+}
